7f7e15fcbce47cc1b53320f91f7ddff9bd9955f5
MattStHilaire <mattsthilaire@ip-192-168-0-14.us-west-2.compute.internal>
2024-03-21 00:21:55 -0500

    SEND IN ZEE IMAGESSSSSS!

--- tim_allen_4ever/app.py
+++ tim_allen_4ever/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    images = [f"tim-allen-{i}.jpeg" for i in range(1, 13)]
+    return render_template("index.html", images=images)
 
 if __name__ == "__main__":
     app.run(debug=True)
